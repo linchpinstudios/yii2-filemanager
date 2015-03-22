9a0fd658ed3dcb9be798a444a87d0b8c0253f2db
Josh Hagel <jhagel@14four.com>
2015-03-22 11:28:24 -0700

    update

--- widgets/Fileupload.php
+++ widgets/Fileupload.php
@@ -12,7 +12,7 @@ use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 /**
- * Use this plugin to unobtrusively add a datetimepicker, datepicker or 
+ * Use this plugin to unobtrusively add a datetimepicker, datepicker or
  * timepicker dropdown to your forms. It's easy to customize options.
  *
  * For example,
@@ -31,7 +31,7 @@ use yii\widgets\InputWidget;
  * @author Josh Hagel <joshhagel@linchpinstudios.com>
  * @since 0.1
  */
- 
+
  class Fileupload extends InputWidget
  {
     /**
@@ -40,30 +40,31 @@ use yii\widgets\InputWidget;
      * @see http://xdsoft.net/jqplugins/datetimepicker/
      */
     public $clientOptions = [];
-    
-    
-    
-    
+
+
+
+
     /**
      * run function.
-     * 
+     *
      * @access public
      * @return void
      */
     public function run()
     {
-        
+
         $randomId = uniqid();
-    
+
         if ($this->hasModel()) {
             echo Html::activeHiddenInput($this->model, $this->attribute, $this->options);
         } else {
             echo Html::hiddenInput($this->name, $this->value, $this->options);
         }
         echo Html::tag('div',Html::a('Select Image',['filemanager/files/filemodal'],['class' => 'btn btn-success','data-toggle' => 'modal','data-target' => '#filePickModal_'.$randomId]));
-        
+
         echo $this->generateModal($randomId);
-        //$this->registerClientScript();
+
+        $this->registerClientScript();
     }
 
 
@@ -82,21 +83,21 @@ use yii\widgets\InputWidget;
 
         $view->registerJs("jQuery('#$id').datetimepicker($options);");*/
     }
-    
-    
-    
-    
+
+
+
+
     protected function generateModal($id)
     {
         $html = '<div class="modal fade" id="filePickModal_'.$id.'">
-                  <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                      ...
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+
+                        </div>
                     </div>
-                  </div>
                 </div>';
-                
+
         return $html;
     }
-    
+
  }
