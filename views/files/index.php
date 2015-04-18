b5f440da52c96fcfb34142f03432d0c26d75b93f
Josh Hagel <joshhagel@linchpinstudios.com>
2015-04-17 21:38:49 -0700

    prepend

--- views/files/index.php
+++ views/files/index.php
@@ -88,7 +88,7 @@ if($awsConfig['enable']){
                                         console.log(data);
                                         $.each(data.result.files, function( index, value ){
                                             console.log(value);
-                                            $("#fileGridManager .row").prependTo(\'<div class="col-xs-6 col-sm-4 col-md-3 image-thumbnail"><a class="thumbnail" href="' . Url::to(['view']) . '?id=\' + value.id + \'" data-id="\' + value.id + \'"><img src="\' + value.thumbnailUrl + \'"></a></div>\');
+                                            $("#fileGridManager .row").prepend(\'<div class="col-xs-6 col-sm-4 col-md-3 image-thumbnail"><a class="thumbnail" href="' . Url::to(['view']) . '?id=\' + value.id + \'" data-id="\' + value.id + \'"><img src="\' + value.thumbnailUrl + \'"></a></div>\');
                                             $(\'#filemanagerUpload\').hide();
                                             $(\'#fileGridManager,#fileGridFooter\').show();
                                         });
